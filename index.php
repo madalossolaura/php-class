4d788dbfa0c4fd03444a4f6d607c7fc044d8b1c7
Laura Madalosso <madalossolaura@gmail.com>
2019-04-02 22:25:09 -0300

    Foreach para os novos cursos

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
-<?php 
-    $nomeCurso1 = "Full Stack";
-    $descricaoCurso1 = "Curso de desenvolvimento web";
-    $valorCurso1 = 1000.99;
-    $imagemCurso1 = "full.jpeg";
-
-    $nomeCurso2 = "Marketing";
-    $descricaoCurso2 = "Curso de desenvolvimento de campanhas";
-    $valorCurso2 = 1000.98;
-    $imagemCurso2 = "marketing.jpg";
+<?php   
+  $cursos = [
+        "Full Stack" => ["Curso de desenvolvimento web", 1000.99, "full.jpeg", "fullstack"],
+        "Marketing" => ["Curso de desenvolvimento de campanhas", 1000.98, "marketing.jpg", "marketing"],
+        "UX" => ["Curso de User Experience", 9000.98, "ux.jpeg", "ux"],
+        "Mobile Android" => ["Curso de apps", 1000, "android.png", "android"]
+  ];  
 
   $usuario = [
         "Nome" => "Laura",
         "Email" => "teste@teste.com",
         "Senha" => "123456",
         "NivelAcesso" => 1
-    ]
+  ];
 ?>
 
 <!DOCTYPE html>
@@ -71,29 +68,64 @@
    <div class="container">
     <!-- começo do card de cursos -->
         <div class="row">
-            <div class="col-sm-6 col-md-6">
-                <div class="thumbnail">
-                <img src="<?php echo "assets/img/$imagemCurso1"; ?>" alt="<?php echo "Foto curso $nomeCurso1" ?>">
-                <div class="caption">
-                    <h3><?php echo $nomeCurso1; ?></h3>
-                    <p><?php echo $descricaoCurso1; ?></p>
-                    <p><?php echo $valorCurso1; ?></p>
-                    <a href="#" class="btn btn-primary" role="button">Comprar</a>
+            <?php foreach ($cursos as $nomeCurso => $infoCurso) : ?>
+                <div class="col-sm-6 col-md-6">
+                    <div class="thumbnail">
+                    <img src="<?php echo "assets/img/$infoCurso[2]"; ?>" alt="<?php echo "Foto curso $infoCurso[2]" ?>">
+                    <div class="caption">
+                        <h3><?php echo $nomeCurso; ?></h3>
+                        <p><?php echo $infoCurso[0]; ?></p>
+                        <p><?php echo $infoCurso[1]; ?></p>
+                        <a href="#" class="btn btn-info" role="button"  data-toggle="modal" data-target="#<?php echo $infoCurso[3] ?>">Comprar</a>
+                    </div>
+                    </div>
                 </div>
+            <?php endforeach ?>
+        </div>
+
+        <?php foreach ($cursos as $nomeCurso => $infoCurso) : ?>
+        <!-- Modal -->
+        <div class="modal fade" id="<?php echo $infoCurso[3] ?>" role="dialog">
+            <div class="modal-dialog">
+            
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Preencha os seus dados</h4>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="input-group col-md-5">
+                            <label for="nomeCompleto">Nome Completo</label>
+                            <input id="nomeCompleto" class="form-control" type="text">
+                        </div>    
+                        <div class="input-group col-md-5">
+                            <label for="cpf">CPF</label>
+                            <input id="cpf" class="form-control" type="number">
+                        </div> 
+                        <div class="input-group col-md-5">
+                            <label for="nroCartao">Numero do Cartão</label>
+                            <input id="nroCartao" class="form-control" type="number">
+                        </div> 
+                        <div class="input-group col-md-5">
+                            <label for="validade">Validade</label>
+                            <input id="validade" class="form-control" type="month">
+                        </div> 
+                        <div class="input-group col-md-5">
+                            <label for="cvv">CVV</label>
+                            <input id="cvv" class="form-control" type="number">
+                        </div> 
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 </div>
             </div>
-            <div class="col-sm-6 col-md-6">
-                <div class="thumbnail">
-                <img src="<?php echo "assets/img/$imagemCurso2"; ?>" alt="<?php echo "Foto curso $nomeCurso2" ?>">
-                <div class="caption">
-                    <h3><?php echo $nomeCurso2; ?></h3>
-                    <p><?php echo $descricaoCurso2; ?></p>
-                    <p><?php echo $valorCurso2; ?></p>
-                    <a href="#" class="btn btn-primary" role="button">Comprar</a>
-                </div>
-                </div>
+            
             </div>
         </div>
+        <?php endforeach ?>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
