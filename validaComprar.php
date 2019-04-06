8ebbe4724918ab1a50b36e2f54010eb07f663e45
Laura Madalosso <madalossolaura@gmail.com>
2019-04-06 12:18:24 -0300

    validacao do form

--- validaComprar.php
+++ validaComprar.php
@@ -1,11 +1,66 @@
 <?php 
-$nome = $_REQUEST["nomeCompleto"];
-$cpf = $_REQUEST["cpf"];
-$cartao = $_REQUEST["nroCartao"];
-$validade = $_REQUEST["validade"];
-$cvv = $_REQUEST["cvv"];
-$nomeCurso = $_REQUEST["nomeCurso"];
-$infoCurso = $_REQUEST["precoCurso"];
+    // var
+    $nome = $_REQUEST["nomeCompleto"];
+    $cpf = $_REQUEST["cpf"];
+    $cartao = $_REQUEST["nroCartao"];
+    $validade = $_REQUEST["validade"];
+    $cvv = $_REQUEST["cvv"];
+    $nomeCurso = $_REQUEST["nomeCurso"];
+    $infoCurso = $_REQUEST["precoCurso"];
+    $erros = [];
+
+    // funções 
+    function validarNome($nome) {
+        return strlen($nome) > 0 && strlen($nome) <= 15;
+    }
+    function validarCpf($cpf) {
+        return strlen($cpf) == 11;
+    }
+    function validarNroCartao($cartao) {
+        $primeiroNum = substr($cartao, 0, 1);
+        return $primeiroNum == 4 || $primeiroNum == 5 || $primeiroNum ==6;
+    }
+    // modo fácil 
+    function validarData($data) {
+        $dataAtual = date("Y-m");
+        return $data >= $dataAtual;
+    }
+
+    // modo enrolado 
+    /*function validarData($data) {
+        $today = getdate();
+        $today["year"];
+        $mes = substr($data, 5, 2);
+        $ano = substr($data, 0, 4);
+        $nmonth = date('m', strtotime($today['month']));
+        return $nmonth < $mes && $today["year"] <= $ano;
+    }*/
+
+    function validarCvv($cvv) {
+        return strlen($cvv) == 3;
+    }
+
+    function validarCompra($nome, $cpf, $cartao, $data, $cvv) {
+        global $erros;
+
+        if (!validarNome($nome)) {
+            array_push($erros, "Preencha seu nome");
+        }
+        if (!validarCpf($cpf)) {
+            array_push($erros, "CPF precisa ter 11 caracteres");
+        }
+        if (!validarNroCartao($cartao)) {
+            array_push($erros, "Seu cartão precisa começar com 4, 5 ou 6");
+        }
+        if (!validarData($data)) {
+            array_push($erros, "A data não é valida");
+        }
+        if (!validarCvv($cvv)) {
+            array_push($erros, "CVV invalido");
+        }
+    }
+
+    validarCompra($nome, $cpf, $cartao, $data, $cvv)
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,25 +76,49 @@ $infoCurso = $_REQUEST["precoCurso"];
 <body>
     <div class="container">
         <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <h3>Compra Realizado com Sucesso!</h3>
-                </div>
-              
-                <div class="panel-body">
-                    <ul class="list-group">
-                        <li class="list-group-item"><strong>Nome Curso: <?php echo $nomeCurso ?></strong></li>
-                        <li class="list-group-item"><strong>Preço: R$ <?php echo $infoCurso ?></strong></li>
-                        <li class="list-group-item"><strong>Nome Completo: <?php echo $nome ?></strong></li>
-                    </ul>
-                    <div class="center">
-                        <a href="index.php">
-                            <span>Voltar para a página inicial >></span>
-                        </a>
+            <?php if (count($erros) > 0) :?>
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h3>Preencha seus dados corretamente</h3>
                     </div>
-                </div>    
-              
-            </div>
+                
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <?php foreach ($erros as $chave => $valorErro) : ?>
+                                <li class="list-group-item">
+                                    <?= $valorErro?>
+                                </li>
+                            <?php endforeach ?>
+                        </ul>
+                        <div class="center">
+                            <a href="index.php">
+                                <span>Voltar para a página inicial >></span>
+                            </a>
+                        </div>
+                    </div>    
+                
+                </div>
+            <?php else : ?>    
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3>Compra Realizado com Sucesso!</h3>
+                    </div>
+                
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><strong>Nome Curso: <?php echo $nomeCurso ?></strong></li>
+                            <li class="list-group-item"><strong>Preço: R$ <?php echo $infoCurso ?></strong></li>
+                            <li class="list-group-item"><strong>Nome Completo: <?php echo $nome ?></strong></li>
+                        </ul>
+                        <div class="center">
+                            <a href="index.php">
+                                <span>Voltar para a página inicial >></span>
+                            </a>
+                        </div>
+                    </div>    
+                
+                </div>
+            <?php endif; ?>    
         </div>   
     </div>
 </body>
